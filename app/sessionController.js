79a843adf8a4a4acaba1279634b55d0e11048839
Jungtae JI <Jungtae JI>
2016-12-07 23:23:58 +1100

    factory added

--- app/sessionController.js
+++ app/sessionController.js
@@ -1,22 +1,46 @@
-angular.module('app').controller('sessionController', [ 'sessionService',
-    function sessionController (sessionService){
-        var vm = this;
-        
-        vm.getServiceSession = function (){
-            vm.model = {
-                name: sessionService.get('name'),
-                nickname: sessionService.get('nickname'),
-                status: 'Retrieved by service on ' + new Date()
-            };
-        }
-        vm.setServiceSession = function (){
-            sessionService.save('name', vm.model.name);
-            sessionService.save('nickname', vm.model.nickname);
-            vm.getServiceSession();
-        }
-        vm.clearServiceSession = function (){
-            sessionService.clear();
-            vm.getServiceSession();
-        }    
+angular.module('app').controller('sessionController', [ 'sessionService', 'sessionFactory', sessionController ]);
+function sessionController (sessionService, sessionFactory){
+    var vm = this;
+    // var mySessionFactory = sessionFactory;
+    vm.getFactorySession = getFactorySession;
+    vm.setFactorySession = setFactorySession;
+    vm.clearFactorySession = clearFactorySession;
+
+    function getFactorySession() {
+        vm.model = {
+            name: sessionFactory.get('name'),
+            nickname: sessionFactory.get('nickname'),
+            status: 'Retrieved by Factory on ' + new Date()
+        };
     }
-])
+
+    function setFactorySession() {
+        sessionFactory.save('name', vm.model.name);
+        sessionFactory.save('nickname', vm.model.nickname);
+        getFactorySession();
+    }
+
+    function clearFactorySession(){
+        sessionFactory.clear();
+        getFactorySession();
+    }
+    
+    vm.getServiceSession = function (){
+        vm.model = {
+            name: sessionService.get('name'),
+            nickname: sessionService.get('nickname'),
+            status: 'Retrieved by service on ' + new Date()
+        };
+    }
+
+    vm.setServiceSession = function (){
+        sessionService.save('name', vm.model.name);
+        sessionService.save('nickname', vm.model.nickname);
+        vm.getServiceSession();
+    }
+
+    vm.clearServiceSession = function (){
+        sessionService.clear();
+        vm.getServiceSession();
+    }    
+}
